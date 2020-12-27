561ce6edc07609bffd0e1dec44b57ee1f84de607
Jeremy Powell <jeremy.wayne.powell@gmail.com>
2020-12-26 23:07:25 -0600

    added dimacs parsing for solver

--- src/solver.rs
+++ src/solver.rs
@@ -10,7 +10,25 @@ pub enum Status {
 }
 
 pub struct Solver {
+    clauses: Vec<Vec<i32>>,
     status: Status,
+    stack: Vec<Frame>,
+    assignments: Vec<Assignment>,
+    next_clause_index: usize,
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Assignment {
+    value: Option<bool>,
+    resolve_frame: Option<usize>,
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Frame {
+    satisfied: bool,
+    clause_index: usize,
+    guess_count: usize,
+    resolve_frame: Option<usize>,
 }
 
 impl Solver {
@@ -27,8 +45,41 @@ impl Solver {
     }
 
     pub fn from_dimacs<R: BufRead>(mut dimacs: Dimacs<R>) -> Solver {
-        Solver {
+        let mut solver = Solver {
+            clauses: vec![],
             status: Status::Unknown,
+            stack: vec![],
+            assignments: vec![],
+            next_clause_index: 0,
+        };
+
+        let mut max = 0;
+        let mut literals = vec![];
+
+        while dimacs.next(&mut literals) {
+            literals.sort_unstable();
+            literals.dedup();
+
+            max = std::cmp::max(
+                literals.iter().map(|l| l.abs() as usize).max().unwrap(),
+                max,
+            );
+
+            solver.clauses.push(literals.clone());
         }
+
+        solver.clauses.sort_unstable();
+        solver.clauses.dedup();
+        solver.clauses.sort_unstable_by_key(|clause| clause.len());
+
+        solver.assignments.extend(
+            std::iter::repeat_with(|| Assignment {
+                value: None,
+                resolve_frame: None,
+            })
+            .take(max + 1),
+        );
+
+        solver
     }
 }
